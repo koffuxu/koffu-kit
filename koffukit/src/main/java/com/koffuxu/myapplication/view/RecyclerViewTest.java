bc08cee33a75df5658fe691f6d6df73c6949ea1d
Koffu Xu <koffuxu@gmail.com>
2017-11-02 17:07:55 +0800

    add recyclerview onclick event

--- koffukit/src/main/java/com/koffuxu/myapplication/view/RecyclerViewTest.java
+++ koffukit/src/main/java/com/koffuxu/myapplication/view/RecyclerViewTest.java
@@ -11,7 +11,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.koffuxu.myapplication.R;
 
@@ -22,6 +24,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 /**
  * Created by koffuxu on 2017/10/31.
  * http://www.androidchina.net/6160.html
@@ -31,6 +35,7 @@ public class RecyclerViewTest extends Activity {
     private static final String TAG = "RecyclerViewTest";
     private RecyclerView mRecyclerView;
     private List<Integer> mDatas;
+    private MyRecyclerAdatper mAdapter;
 
 
 
@@ -43,10 +48,20 @@ public class RecyclerViewTest extends Activity {
         //设置布局管理器
         LinearLayoutManager llm = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(llm);
-        //llm.setOrientation(LinearLayoutManager.VERTICAL);
+        llm.setOrientation(LinearLayoutManager.HORIZONTAL);
 
-        //设置适配器
-        mRecyclerView.setAdapter(new MyRecyclerAdatper(this,mDatas));
+        //设置适配器,把自定义的数据mDatas传入
+        mAdapter = new MyRecyclerAdatper(this, mDatas);
+        mRecyclerView.setAdapter(mAdapter);
+
+        //设置每个item监听事件
+        mAdapter.setmItemClickListener(new OnMyItemClickListener() {
+            @Override
+            public void onMyItemClick(View view, int postion) {
+                Toast.makeText(RecyclerViewTest.this, "点击了第 "+postion+" 个ITEM",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -58,16 +73,27 @@ public class RecyclerViewTest extends Activity {
 
     }
 
-    private class MyRecyclerAdatper extends RecyclerView.Adapter<MyRecyclerAdatper.MyHolder> {
+    //item的回调接口
+    public interface OnMyItemClickListener{
+        void onMyItemClick(View view, int postion);
+    }
+    //继承自泛型为自定义MyHoler的Adapter
+    public class MyRecyclerAdatper extends RecyclerView.Adapter<MyRecyclerAdatper.MyHolder> {
         private Context mContext;
         private List<Integer> mDatas;
+        private OnMyItemClickListener mItemClickListener;
 
+        //构造函数
         public MyRecyclerAdatper(Context context, List<Integer> datas){
             super();
             this.mContext = context;
             this.mDatas = datas;
         }
 
+        //设置回调接口函数
+        public void setmItemClickListener(OnMyItemClickListener mItemClickListener) {
+            this.mItemClickListener = mItemClickListener;
+        }
 
         @Override
         public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -77,25 +103,38 @@ public class RecyclerViewTest extends Activity {
             return myHolder;
         }
 
-        class MyHolder extends RecyclerView.ViewHolder{
-            ImageView imageView;
-            public MyHolder(View v){
-                super(v);
-                imageView = (ImageView)v.findViewById(R.id.iv_item);
-            }
-        }
+
 
 
         @Override
-        public void onBindViewHolder(MyHolder holder, int position) {
+        public void onBindViewHolder(final MyHolder holder, final int position) {
             holder.imageView.setImageResource(mDatas.get(position));
             //holder.imageView.setImageResource(R.drawable.pet);
+            //回调处理
+            if(mItemClickListener != null){
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mItemClickListener.onMyItemClick(holder.itemView, position);
+                    }
+                });
+            }
 
         }
 
         @Override
         public int getItemCount() {
             return mDatas.size();
+        }
+
+
+        //内部类MyHolder，来获得item中的控件
+        class MyHolder extends RecyclerView.ViewHolder{
+            ImageView imageView;
+            public MyHolder(View v){
+                super(v);
+                imageView = (ImageView)v.findViewById(R.id.iv_item);
+            }
         }
     }
 }
